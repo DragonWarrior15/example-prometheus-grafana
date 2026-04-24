54315745ad6a3a6a321e10155ee1622238052802
Vaibhav Ojha <DragonWarrior15@users.noreply.github.com>
2026-04-24 21:49:30 +0530

    add gauge metric (#4)

--- main.py
+++ main.py
@@ -1,10 +1,12 @@
 import random
 import time
 
-from prometheus_client import start_http_server, Summary
+from prometheus_client import Gauge, start_http_server, Summary
 
 # Create a metric to track time spent and requests made.
 REQUEST_TIME = Summary("request_processing_seconds", "Time spent processing request")
+ACTIVE_USERS_GAUGE = Gauge("active_users_count", "Current active users")
+ACTIVE_USERS_GAUGE_VALUE: int = 0
 
 
 # Decorate function with metric.
@@ -14,9 +16,30 @@ def process_request(t):
     time.sleep(t)
 
 
+def get_active_users():
+    """A dummy function that fetches the current active users."""
+    global ACTIVE_USERS_GAUGE_VALUE
+
+    # we choose randomly if we want to increase or decrease the value
+    if random.random() <= 0.6:
+        # do increment
+        _delta = random.randint(1, 20)
+        ACTIVE_USERS_GAUGE_VALUE += _delta
+        ACTIVE_USERS_GAUGE.inc(_delta)
+    else:
+        # decrease
+        _delta = min(random.randint(1, 20), ACTIVE_USERS_GAUGE_VALUE)
+        ACTIVE_USERS_GAUGE_VALUE -= _delta
+        ACTIVE_USERS_GAUGE.dec(_delta)
+
+    # some sleep timer
+    time.sleep(2)
+
+
 if __name__ == "__main__":
     # Start up the server to expose the metrics.
     start_http_server(8000)
     # Generate some requests.
     while True:
         process_request(random.random())
+        get_active_users()
